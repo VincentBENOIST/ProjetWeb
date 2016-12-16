3a3e54363d16b4ee0f28392e0b38124f8161562c
Adrien BRISSET <adrien.brisset@etudiant.univ-lille1.fr>
2016-12-16 09:28:40 +0100

    toto

---
+++ src/users_package/ListerAbscencesEtu.java
@@ -0,0 +1,38 @@
+package users_package;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ * Servlet implementation class ListerAbscencesEtu
+ */
+@WebServlet("/ListerAbscencesEtu")
+public class ListerAbscencesEtu extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	protected void service(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+		HttpSession session = req.getSession(true);
+		if (session.getAttribute("client") == null || !(session.getAttribute("client") instanceof Secretaire)) {
+			// dire erreur
+			res.sendRedirect("../index.html");
+		} else {
+			PrintWriter out = res.getWriter();
+			out.print("<!DOCTYPE html>" + "<html>" + "<head>" + "<title>Authentification</title>"
+					+ "<!--source : Bootsnip.com -->" + "<meta charset='utf-8'>"
+					+ "<meta http-equiv='X-UA-Compatible' content='IE=edge'>"
+					+ "<meta name='viewport' content='width=device-width, initial-scale=1'>"
+					+ "<link rel='stylesheet' href='CSS/bootstrap-3.3.7-dist/css/bootstrap.min.css'>"
+					+ "<link rel='stylesheet' href='CSS/font-awesome-4.7.0/css/font-awesome.min.css'>"
+					+ "<link rel='stylesheet' href='CSS/style.css'>" + "</head>");
+		}
+	}
+
+}
--- src/verif_client/Verif_Authent.java
+++ src/verif_client/Verif_Authent.java
@@ -1,18 +1,23 @@
 package verif_client;
 
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import org.apache.commons.lang3.StringEscapeUtils;
 
 import users_package.Etudiant;
 import users_package.Professeur;
 import users_package.Secretaire;
-
 
 /**
  * Servlet implementation class Verif_Authent
@@ -21,20 +26,54 @@ import users_package.Secretaire;
 public class Verif_Authent extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void service(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		String login = request.getParameter("login");
-		String mdp =  request.getParameter("password");
+		String mdp = request.getParameter("password");
 		login = StringEscapeUtils.escapeHtml4(login);
 		mdp = StringEscapeUtils.escapeHtml4(mdp);
-		/**
-		 * Connection a la base
-		 */
-		// si le resultat dit que cette personne est un etudiant
-		request.setAttribute("client",new Etudiant(login, mdp));
-		//si cette personne est une secrataire
-		request.setAttribute("client", new Secretaire(login, mdp));
-		//si cette personne est un prof
-		request.setAttribute("client", new Professeur(login, mdp));
+		try {
+			Class.forName("org.postgresql.Driver");
+
+			String url = "jdbc:postgresql://psqlserv/n3p1";
+			String nom = "benoistv";
+			String passwd = "moi";
+			Connection con = DriverManager.getConnection(url, nom, passwd);
+
+			Statement state = con.createStatement();
+			String query = "SELECT * from personne where login='" + login + "' and mdp='" + mdp + "'";
+			ResultSet rs = state.executeQuery(query);
+
+			if (rs.next()) {
+				System.out.println("ok");
+				HttpSession session = request.getSession(true);
+				System.out.println(rs.getString(5));
+				if (rs.getString(5).equals("secretaire")) {
+					// si cette personne est une secrataire
+					session.setAttribute("client", new Secretaire(login, mdp));
+					response.sendRedirect(
+							/* page liste abscences avec modif justif */"");
+				} else if (rs.getString(5).equals("etudiant")) {
+					// si le resultat dit que cette personne est un etudiant
+					session.setAttribute("client", new Etudiant(login, mdp));
+					response.sendRedirect(/* page liste abscences */"");
+				} else if (rs.getString(5).equals("prof")) {
+					// si cette personne est un prof
+					session.setAttribute("client", new Professeur(login, mdp));
+					response.sendRedirect(/* page ajout abscences */"");
+				} else {
+					System.out.println("redirect");
+					response.sendRedirect("index.html");
+				}
+
+			} else {
+				response.sendRedirect("index.html");
+			}
+			con.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
 	}
 
 }
