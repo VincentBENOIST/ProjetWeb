fb1eb9c667bf7238ef828b199abda426c83871fe
Vincent Benoist <24101997o@gmail.com>
2016-12-16 09:26:23 +0100

    index fail, sql

--- SQL/scriptCreateTables.sql
+++ SQL/scriptCreateTables.sql
@@ -1,47 +1,52 @@
-CREATE TABLE etudiant(
+drop table justificatif;
+drop table absence;
+drop table personne;
+
+
+
+CREATE TABLE personne(
 	-- les attributs 
-	eno integer ,
+	login text,
 	nom text ,
 	prenom text ,
-	groupe varchar(1) ,
-	primary key(eno)
-);
-CREATE TABLE prof(
-	-- les attributs
-	pno integer ,
-	nom text ,
-	prenom text ,
-	primary key(pno)
-);
-CREATE TABLE secretaire(
-	-- les attributs
-	sno integer ,
-	nom text ,
-	prenom text ,
-	primary key(sno)
-);
-CREATE TABLE justificatif(
-	eno integer ,
-	dateDebut timestamp ,
-	dateFin timestamp ,
-	primary key(eno),
-	foreign key(eno) references etudiant(eno)
+	mdp text,
+	groupe text ,
+	primary key(login)
 );
 
-CREATE TABLE abscence (
+CREATE TABLE justificatif(
+	jno integer,
+	libelle text,
+	dateDebut date ,
+	dateFin date ,
+	loginsecretaire text,
+	loginetudiant text,
+	primary key(jno),
+	foreign key(loginsecretaire) references personne(login),
+	foreign key(loginetudiant) references personne(login)
+);
+
+CREATE TABLE absence (
 	-- les attributs 
-	ano serial ,
-	nom text ,
-	prenom text ,
-	groupe varchar(1),
-	dateDebut timestamp ,
-	dateFin timestamp ,
+	ano integer,
+	login text,
+	dateDebut date ,
+	dateFin date ,
 	-- les references :
 	primary key(ano) ,
-	foreign key(nom) references etudiant(nom) , 
-	foreign key(prenom) references etudiant(prenom) ,
-	foreign key(groupe) references etudiant(groupe) ,
-	foreign key(dateDebut) references justificatif(dateDebut),
-	foreign key(dateFin) references justificatif(dateFin)
-
+	foreign key(login) references personne(login)
+	
 );
+
+insert into personne values ('brisseta','brisset','adrien','brad','etudiant');
+insert into personne values ('benoistv','benoist','vincent','bevi','etudiant');
+insert into personne values ('vanderar','vanderaspoilden','rodolphe','varo','etudiant');
+insert into personne values ('dillese','dillies','elodie','diel','secretaire');
+insert into personne values ('beaufilb','beaufil','bruno','bebr','prof');
+insert into justificatif values (1,'malade','2016-12-27','2016-12-28','dillese','vanderar');
+insert into justificatif values (2,'retard','2017-02-20','2017-02-21','dillese','vanderar');
+insert into justificatif values (3,'panne','2016-12-20','2016-12-22','dillese','benoistv');
+insert into absence values (1,'vanderar','2016-12-27','2016-12-28');
+insert into absence values (2,'vanderar','2017-02-20','2017-02-21');
+insert into absence values (3,'vanderar','2016-12-25','2016-12-26');
+insert into absence values (4,'benoistv','2016-12-20','2016-12-22');
